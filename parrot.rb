0785f5f57c8b2359e62c40f9580655c2c2cf828c
Daniel Kim <daniel.kim52@gmail.com>
2020-02-14 09:22:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase="Squawk!")
-  puts "#{phrase}"
+def parrot(hi="Squawk!")
+  puts "#{hi}"
 end
-
-parrot(phrase)
